1cb1124698f417ad0673beb2a1ca6e4411430dbc
unknown <ariprasath3@gmail.com>
2021-06-10 07:42:51 +0530

    added: similating to find the winner for 10 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -x
-
-result=$((RANDOM%2));
-if [ $result -eq 0 ];
-then
-	echo "Head is the winner";
-elif [ $result -eq 1 ];
-then
-	echo "Tail is the winner";
-fi
+count=1
+while [ $count -le 10 ]
+do
+	result=$((RANDOM%2));
+	if [ $result -eq 0 ];
+	then
+		echo "Head is the winner";
+	elif [ $result -eq 1 ];
+	then
+		echo "Tail is the winner";
+	fi
+count=$(($count+1));
+done
